be3f4a070d234b117dd72592fc9f1d287e0aa01e
Christian Zenker <typo3@xopn.de>
2014-10-03 21:10:58 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'site-typo3org'
+maintainer       'Christian Zenker'
+maintainer_email 'typo3@xopn.de'
+license          'All rights reserved'
+description      'Installs/Configures typo3.org'
+long_description 'Installs/Configures typo3.org'
+version          '0.0.0'
+
+depends 'apache2', '~> 2.0.0'
+depends 'php', '~> 1.4.6'
